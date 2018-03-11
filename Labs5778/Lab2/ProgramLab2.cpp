a84380281feeef5ac933c500d8f153c14f0aeb6e
Eliezer Gensburger <eliezer.jct@gmail.com>
2018-03-11 12:00:46 +0200

    Add files via upload

---
+++ Labs5778/Lab2/ProgramLab2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>  // STL
+# include "Date.h"
+using namespace std;
+
+int main()
+{
+	list<Date> mylist;		// STL container
+//
+	mylist.push_back(Date(11, 3, 2018));
+	Date myarray[17];
+
+	// TO DO
+
+	return 0;
+}
