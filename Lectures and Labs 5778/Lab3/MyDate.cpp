12567cf664a28c59d7b67d8b4bf2f7a3a6a9f49d
M L <meirl@g.jct.ac.il>
2018-04-15 15:12:00 +0300

    MyDate update

--- Lectures and Labs 5778/Lab3/MyDate.cpp
+++ Lectures and Labs 5778/Lab3/MyDate.cpp
@@ -2,11 +2,56 @@
 
 
 
-MyDate::MyDate()
+bool MyDate::CheckConstrait(int num, int max)
 {
+	if (num <= max)
+		return true;
+	return false;
+}
+
+MyDate::MyDate(int d,int m,int y)
+{
+	day = d;
+	month = m;
+	year = y;
 }
 
 
 MyDate::~MyDate()
 {
+}
+
+void MyDate::SetDay(int d)
+{
+	day = d;
+}
+
+void MyDate::SetMonth(int m)
+{
+	month = m;
+}
+
+void MyDate::SetYear(int y)
+{
+	year = y;
+}
+
+int MyDate::GetDay()
+{
+	return day;
+}
+
+int MyDate::GetMonth()
+{
+	return month;
+}
+
+int MyDate::GetYear()
+{
+	return year;
+}
+
+bool MyDate::CheckDate(MyDate da)
+{
+	return (CheckConstrait(da.day,31) && CheckConstrait(da.month,12) && CheckConstrait(da.year,9999));
 }
--- Lectures and Labs 5778/Lab3/MyDate.h
+++ Lectures and Labs 5778/Lab3/MyDate.h
@@ -4,10 +4,14 @@ class MyDate
 	int day, month, year;
 	bool CheckConstrait(int num, int max);
 public:
-	MyDate();
+	MyDate(int d=1,int m=1,int y=1970);
 	~MyDate();
 	void SetDay(int);
 	void SetMonth(int);
 	void SetYear(int);
+	int GetDay();
+	int GetMonth();
+	int GetYear();
+	bool CheckDate(MyDate);
 };
 
