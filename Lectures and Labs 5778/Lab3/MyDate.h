a950d2950ed2e7e2741b9a9dfb5e5a49d011667f
M L <meirl@g.jct.ac.il>
2018-04-15 14:55:20 +0300

    MyDate added Attributes

--- Lectures and Labs 5778/Lab3/MyDate.h
+++ Lectures and Labs 5778/Lab3/MyDate.h
@@ -1,8 +1,13 @@
 #pragma once
 class MyDate
 {
+	int day, month, year;
+	bool CheckConstrait(int num, int max);
 public:
 	MyDate();
 	~MyDate();
+	void SetDay(int);
+	void SetMonth(int);
+	void SetYear(int);
 };
 
