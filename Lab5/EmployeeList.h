1472f9407ca795761ae251d6b850c39bab456a73
eliezergensburger <eliezer.jct@gmail.com>
2018-05-01 15:19:48 +0300

    add class : EmpoyeeList: public list<Employee*>

---
+++ Lab5/EmployeeList.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Employee.h"
+#include "Seller.h"
+#include "Technician.h"
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+class EmployeeList :public list<Employee*>
+{
+public:
+	EmployeeList();
+	void pop_front();
+	~EmployeeList();
+};
+
+
+
+EmployeeList::EmployeeList() :list<Employee*>()
+{
+}
+
+inline void EmployeeList::pop_front()
+{
+	Employee* ptr = this->front();
+	list<Employee*>::pop_front();
+	delete ptr;
+}
+
+
+EmployeeList::~EmployeeList()
+{
+	for (auto elem : *this)
+	{
+		delete elem;
+	}
+}
